9a89756bde18ebe308806dc045279b21801f6695
XuanLocLuong <lexuanloc26022004@gmail.com>
2026-07-12 22:38:54 +0700

    [LocLX] Remove importing redundantly

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import './App.css'
-import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 import {AuthProvider} from "./contexts/AuthContext.jsx";
 import {BrowserRouter} from "react-router-dom";
